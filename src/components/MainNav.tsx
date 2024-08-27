34b8000b9595a9e020a6ef7da3fe28e84b384d66
Okpube victor <curiousv32@gmail.com>
2024-08-27 03:35:54 -0500

    updated MainNav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,7 +1,7 @@
  import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
-import UsernameMenu from "@/UsernameMenu";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
    const { loginWithRedirect, isAuthenticated } = useAuth0();
